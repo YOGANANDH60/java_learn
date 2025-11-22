039541d086ff5856d7f202cf4f4d46b7b6125f9e
YOGANANDH60 <yoga132005@gmail.com>
2025-11-22 19:34:03 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,15 @@
+class first{
+    public static void main (String args[]){
+        // System.out.println("hello buddy............");
+        // int a= 3;
+        // char b='g';
+        // System.out.println(a+b);
+        byte a = 10;
+        byte b = 20;
+        // type promotion
+            int result = a*b;
+            System.out.println(result);
+
+    }
+}
+
