8944649ffa55b332e2e5eb676ce2aa9a880dd466
YOGANANDH60 <yoga132005@gmail.com>
2025-11-23 20:44:49 +0530

    commit

---
+++ forr.java
@@ -0,0 +1,17 @@
+public class forr {
+    public static void main(String[] args) {
+        int i =1;
+    //     while (i<=10) {
+    //         System.out.println(i);
+    //         i++;
+    //     }
+    //     System.out.println("-----------------------------------------------------");
+    //     do {
+    //         System.out.println(i);
+    //         i++;
+    //     } while (i<20);
+        for (int j = 0; j <=10; j++) {
+           System.out.println(j+i);
+        }
+    }
+}
