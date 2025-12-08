b255db3345768f10320c19a14d5fddb2832ebd83
YOGANANDH60 <yoga132005@gmail.com>
2025-12-08 23:19:24 +0530

    update user input for 2d array

--- input2d_array.java
+++ input2d_array.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class input2d_array {
@@ -11,11 +13,17 @@ public class input2d_array {
             }
         }
 
-        for(int[] n:arr){
-            for(int m :n){
-                System.out.print(" "+m);
-            }
-            System.out.println();
+        // // enhanced for loop only for array and collectiob
+        // for(int[] n:arr){
+        //     for(int m :n){
+        //         System.out.print(" "+m);
+        //     }
+        //     System.out.println();
+        // }
+
+        // normal fro loop with toString method
+        for(int row =0; row<arr.length;row++){
+            System.out.println(Arrays.toString(arr[row]));
         }
     }
 }
