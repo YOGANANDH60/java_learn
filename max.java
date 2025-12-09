7e1aa3307305703e784f3d82c70c2cea87aac764
YOGANANDH60 <yoga132005@gmail.com>
2025-12-09 22:50:16 +0530

    max element in array

--- max.java
+++ max.java
@@ -3,10 +3,10 @@ public class max {
     public static void main(String[] args) {
         int arr[] = {1,2,8,4,5,};
 
-        System.out.println(max(arr));
+        System.out.println(maxe(arr));
     }
 
-    static int max(int[] arr) {
+    static int maxe(int[] arr) {
         int m = arr[0];
         for (int i = 0 ; i<arr.length;i++){
             if(arr[i]>m){
