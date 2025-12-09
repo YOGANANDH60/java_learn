c91f69100e8b181c38db9639c31c94bf95b1d5b3
YOGANANDH60 <yoga132005@gmail.com>
2025-12-09 21:56:28 +0530

    arraylist

---
+++ arrlist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class arrlist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list  = new ArrayList<>(10);
+
+        list.add(23);
+        list.add(23);
+        list.add(29999);
+        list.add(2888);
+        list.add(27);
+        list.add(26);
+        list.add(25);
+        list.add(24);
+        list.add(24);
+        list.add(24);
+        list.add(24);
+        list.add(24);
+        list.add(24);
+        System.out.println(list);
+    }
+}
