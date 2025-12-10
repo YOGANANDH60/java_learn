bf700daa4879c0ff626acd1e23f37e10baf18466
YOGANANDH60 <yoga132005@gmail.com>
2025-12-10 12:53:13 +0530

    sort the array

---
+++ arraysort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class arraysort {
+   public static void main(String[] args) {
+    int arr[] = {3,2,4,77,1};
+
+    sort(arr);
+    System.out.println(Arrays.toString(arr));
+   }
+
+   static void sort(int[] arr) {
+        int n = arr.length;
+        while (n>1) {
+            for(int i = 0;i<n-1;i++){
+                if(arr[i]>arr[i+1]){
+                    swapp(arr, i, i+1);
+                }
+            }
+            n--;
+        }
+   } 
+    private static void swapp(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp; 
+    }
+}
