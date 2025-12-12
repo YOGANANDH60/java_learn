0d4fb8b28d6238ec44e44d0dd493884d48da8447
YOGANANDH60 <yoga132005@gmail.com>
2025-12-12 22:36:24 +0530

    binary search

--- binary.java
+++ binary.java
@@ -1,6 +1,6 @@
 public class binary {
     public static void main(String[] args) {
-        int[] arr = {1,3,5,6};
+        int[] arr = {1,3,5,6,9};
         int target = 7;
         int s = 0;
         int e = arr.length -1;
@@ -12,22 +12,18 @@ public class binary {
     static int search(int[] arr, int s, int e, int target2) {
          
         if(s>e){
-            return -1;
+            return s;//to where it would be 
+            // return -1;//if not in there in array 
         }
         int m = s+(e-s) / 2;
 
          if (arr[m] == target2 ) {
-            return m;
+            return arr[m];
          }
          if (target2 < arr[m] ) {
             return search(arr, s, m-1, target2);
          }
-         else if (target2 > arr[m]) {
-            return search(arr, m+1, e, target2);
-        }
-        else {
-            int l = m+1;
-            return l;
-        }
+        return search(arr, m+1, e, target2);
+
     }
 }
