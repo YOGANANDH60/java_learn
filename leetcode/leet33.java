632d7b24cd637bd6fae4f4c712da762d8b16d25a
YOGANANDH60 <yoga132005@gmail.com>
2025-12-15 00:20:44 +0530

    leetcode 33

---
+++ leetcode/leet33.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+public class leet33 {
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        int ans = search(arr,target);
+        System.out.println(ans);
+}
+
+    static int search(int[] arr,int target){
+        int s = 0;
+        int e = arr.length-1;
+
+        while (s<= e) {
+            int mid = s+(e-s) /2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            // left side
+
+            if(arr[s]<=arr[mid]){
+                if(target>=arr[s] && target < arr[mid]){
+                    e = mid-1;
+                }
+                else
+                {
+                    s = mid + 1;
+                }
+            }
+            else{
+                if(target>arr[mid] && target <= arr[e]){
+                    s = mid + 1;
+
+                }
+                else
+                {
+                    e = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    
+}
