a80b200d33d0c64ca77c15a385effcf67dac442b
YOGANANDH60 <yoga132005@gmail.com>
2026-01-08 22:59:59 +0530

    adding leed205

---
+++ leetcode/leed205.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class leed205 {
+    public static void main(String[] args) {
+        
+        boolean ans = isIsomorphic("egg", "add");
+    }
+    public static boolean isIsomorphic(String s, String t) {
+        
+        HashSet<String> a = new HashSet<>();
+
+        for(int i =0;i<s.length();i++){
+            
+        }
+
+        return false;
+        
+    }
+}
