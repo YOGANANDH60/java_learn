14a746ba50e6cc3f8b9a1fb43f239c0f9ac103ce
YOGANANDH60 <yoga132005@gmail.com>
2026-01-11 22:25:33 +0530

    string methods

---
+++ string methods/equal.java
@@ -0,0 +1,13 @@
+public class equal {
+    public static void main(String[] args) {
+        // String a = "yoga";
+        // String b = "yoga";
+        // System.out.println(a==b);//true
+        String a = new String("yoga");
+        String b = new String("yoga");
+
+        System.out.println(a.equals(b));
+
+
+    }
+}
