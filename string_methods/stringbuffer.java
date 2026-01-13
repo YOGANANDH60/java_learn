3efabf8c3998dd3e0c11eb8ba3eaff4aa0ec305f
YOGANANDH60 <yoga132005@gmail.com>
2026-01-13 23:03:12 +0530

    string buffer methods

---
+++ string_methods/stringbuffer.java
@@ -0,0 +1,19 @@
+package string_methods;
+
+public class stringbuffer {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer(20);
+
+        sb.append("hello yoga,");
+        sb.append(" how's going");
+
+        System.out.println(sb);
+        System.out.println(sb.capacity());
+        System.out.println(sb.lastIndexOf("o"));
+        System.out.println(sb.charAt(19));
+        System.out.println(sb.length());
+        System.out.println(sb.substring(5,8));
+
+
+    }
+}
